c475b277aa65d7ba3d941f6ffc8ea6ea99b842a6
Gulbis <rgubis41@gmail.com>
2024-11-18 22:38:34 +0200

    Tagad var nokopet no lapas

--- script.js
+++ script.js
@@ -4,36 +4,36 @@ const work = document.getElementById('work');
 const contact = document.getElementById('contact');
 
 function aboutA() {
-    skills.style.opacity = '0';
-    work.style.opacity = '0';
-    contact.style.opacity = '0';
-    about.style.opacity = '1';
+    skills.style.opacity = '0'; skills.style.zIndex = '0';
+    work.style.opacity = '0'; work.style.zIndex = '0';
+    contact.style.opacity = '0'; contact.style.zIndex = '0';
+    about.style.opacity = '1'; about.style.zIndex = '1';
 }
 
 function skillsA() {
-    about.style.opacity = '0';
-    work.style.opacity = '0';
-    contact.style.opacity = '0';
-    skills.style.opacity = '1';
+    about.style.opacity = '0'; about.style.zIndex = '0';
+    work.style.opacity = '0'; work.style.zIndex = '0';
+    contact.style.opacity = '0'; contact.style.zIndex = '0';
+    skills.style.opacity = '1'; skills.style.zIndex = '1';
 }
 
 function workA() {
-    about.style.opacity = '0';
-    skills.style.opacity = '0';
-    contact.style.opacity = '0';
-    work.style.opacity = '1';
+    about.style.opacity = '0'; about.style.zIndex = '0';
+    skills.style.opacity = '0'; skills.style.zIndex = '0';
+    contact.style.opacity = '0'; contact.style.zIndex = '0';
+    work.style.opacity = '1'; work.style.zIndex = '1';
 }
 
 function contactA() {
-    about.style.opacity = '0';
-    skills.style.opacity = '0';
-    work.style.opacity = '0';
-    contact.style.opacity = '1';
+    about.style.opacity = '0'; about.style.zIndex = '0';
+    skills.style.opacity = '0'; skills.style.zIndex = '0';
+    work.style.opacity = '0'; work.style.zIndex = '0';
+    contact.style.opacity = '1'; contact.style.zIndex = '1';
 }
 
 function bide() {
-    about.style.opacity = '0';
-    skills.style.opacity = '0';
-    work.style.opacity = '0';
-    contact.style.opacity = '0';
+    about.style.opacity = '0'; about.style.zIndex = '0';
+    skills.style.opacity = '0'; skills.style.zIndex = '0';
+    work.style.opacity = '0'; work.style.zIndex = '0';
+    contact.style.opacity = '0'; contact.style.zIndex = '0';
 }
